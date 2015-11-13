9a17510c6b42197c174e9cb83f2b7b7c22a96b5b
LiuWenJu <lwj037303@gmail.com>
2015-11-13 16:04:44 +0800

    request and bs4

--- beautiful.py
+++ beautiful.py
@@ -3,12 +3,58 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "http://www.amazon.cn/gp/bestsellers/books/ref=s9_acsd_ri_bw_clnk?pf_rd_m=A1AJ19PSB66TGU&pf_rd_s=merchandised-search-5&pf_rd_r=0Y1F0E7MATQPWDH0MMD0&pf_rd_t=101&pf_rd_p=261616452&pf_rd_i=658390051_p0_h"
-
+url = "http://www.yellowpages.com/atlanta-ga/fire-water-damage-restoration"
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, "lxml")
 
-href = soup.find_all("a")
-for item in href:
-    print item.text, item.get("href")
+links = soup.find_all("a")
+for	item in links:
+    print "<a href='%s'>%s</a>" %(item.get("href"), item.text)
+
+
+data = soup.find_all("div", {"class": "info"})
+
+list = []
+for item in data:
+    try:
+        l1 = item.contents[0].find_all("a", {"class": "business-name"})[0].text + "\n"
+        list.append(l1)
+        print l1
+    except:
+        print "l1 error"
+
+        #with open('test.txt','a+') as f:
+        #    f.writelines(l1)
+
+    try:
+        l2 = item.contents[1].find_all("span", {"itemprop": "streetAddress"})[0].text + "\n"
+        print l2
+        list.append(l2)
+    except:
+        print "l2 error"
+
+#        with open('test.txt','a+') as f:
+#            f.writelines(l2)
+
+    try:
+        l3 = item.contents[1].find_all("div", {"itemprop": "telephone"})[0].text + "\n"
+        print l3
+        list.append(l3)
+    except:
+        print "l3 error"
+
+#        with open('test.txt','a+') as f:
+#            f.writelines(l3)
+    try:
+        l4 = item.contents[1].find_all("span", {"itemprop": "postalCode"})[0].text + "\n"
+        print l4
+        list.append(l4)
+    except:
+        print "l4 error"
+#        with open('test.txt','a+') as f:
+#            f.writelines(l4)
+with open('list.txt', 'w+') as file:
+    for l in list:
+        file.writelines(l)
+
